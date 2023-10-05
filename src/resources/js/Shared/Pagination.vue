d53693ebc15d6a42b8daf66a2ad23e8de981cba6
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-10-05 11:38:25 +0200

    Add pagination

---
+++ src/resources/js/Shared/Pagination.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Component v-for="link in links" :is="link.url ? 'Link' : 'span'" :href="link.url" v-html="link.label" class="px-1"
+            :class="{
+                'text-indigo-600 font-bold underline': link.active,
+                'text-gray-500': !link.url,
+            }" />
+
+        <!-- <template v-for="link in users.links"> -->
+        <!--     <Link v-html="link.label" :href="link.url" v-if="link.url" /> -->
+        <!--     <span v-else v-html="link.label" /> -->
+        <!-- </template> -->
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    links: Array,
+});
+</script>
